238aef11f3b6f845401db8ea053d4c321c2c29b5
Robert Crim <rob@servermilk.com>
2013-02-12 11:31:28 +0000

    Rule to float feh

--- config.h
+++ config.h
@@ -22,6 +22,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "Steam",    NULL,       NULL,       0,            True,        -1 },
+	{ "feh",      NULL,       NULL,       0,            True,        -1 },
 };
 
 /* layout(s) */
