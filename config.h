7cf4bb40813c783cd7e496f5d61fa8a9883f5d36
Robert Crim <rob@servermilk.com>
2014-03-01 09:20:44 +0000

    Gimp isn't special anymore

--- config.h
+++ config.h
@@ -22,7 +22,6 @@ static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
 	{ "Steam",    NULL,       NULL,       0,            True,        -1 },
 	{ "feh",      NULL,       NULL,       0,            True,        -1 },
 };
